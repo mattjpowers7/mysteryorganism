b2a12ea9ee8c49ae2e9502fc581c17f1502eae89
mattjpowers7 <38008156+mattjpowers7@users.noreply.github.com>
2023-08-22 18:13:34 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+
+const pAequorFactory = (num, DNArray) => { return {
+  specimenNum: num,
+  dna: DNArray
+}
+}
+
+console.log(PAequorFactory(15, {}));
+
+
+
+
